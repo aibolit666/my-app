42bf91d55af9800d7f66fd1907071cb25aa93520
aibolit666 <pavel.priladyshev@gmail.com>
2020-12-21 22:11:04 +0300

    change 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://www.clipartmax.com/png/full/217-2178672_free-company-logo-png.png' alt='New Year'></img>
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+        <img src='http://sa-kzn.ru/wp-content/uploads/2019/04/Wiki-background-1024x436.jpg' alt='Шапка сайта'>
+  </img></div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      my posts
+      <div>
+      new post
+    </div>
+    <div>
+      post 1
+    </div>
+    <div>
+      post 2
+    </div>
+    </div>
+  </div>
+}
+
+export default Profile;
